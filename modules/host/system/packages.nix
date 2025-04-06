ae91bcac65065b4a741d099bae78998597a59623
lorem10 <momenei.alireza1382.15@gmail.com>
2025-04-06 16:12:10 +0330

    add proxychains package with simple proxy set

--- modules/host/system/packages.nix
+++ modules/host/system/packages.nix
@@ -7,6 +7,18 @@
   programs.hyprland.xwayland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  programs.proxychains = {
+    enable = true;
+    proxies = {
+      myproxy = {
+        enable = true;
+        type = "socks5";
+        host = "127.0.0.1";
+        port = 1080;
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     wget
     kitty
