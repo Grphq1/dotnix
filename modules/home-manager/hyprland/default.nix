35c698c123bebe6f014e45671127c88f6313ba63
lorem10 <momenei.alireza1382.15@gmail.com>
2025-04-23 10:44:06 +0330

    feat(hyprland): add hyprgrass plugin

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -5,5 +5,8 @@
 }: {
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
   wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.plugins = [
+    pkgs.hyprlandPlugins.hyprgrass
+  ];
   wayland.windowManager.hyprland.extraConfig = builtins.readFile ./hyprland.conf;
 }
